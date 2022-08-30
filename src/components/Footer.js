ed003b77910d67cc11be2a59d089156678e3c577
Juan Ortiz <juanortiz@iMac-de-Juan.local>
2022-08-30 15:43:37 -0500

    Ajuste footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,12 @@ import React from 'react';
 
 function Footer(){
     return(
+        <div>
         <footer>              
         </footer>
+
+        </div>
+        
                     
     )
 }
