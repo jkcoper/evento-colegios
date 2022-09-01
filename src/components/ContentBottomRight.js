cc68c5e16104a9dbc157afb1b4e567cf15c9d4e3
Juan Ortiz <juanortiz@iMac-de-Juan.local>
2022-09-01 08:09:53 -0500

    ajustes finales sponsor

--- src/components/ContentBottomRight.js
+++ src/components/ContentBottomRight.js
@@ -6,10 +6,10 @@ function ContentBottomRight(){
         <div className="detalle">
             <div className="textodetalle">
             <h2 className='titulo1'>Desarrolle competencias en sus estudiantes.</h2>
-            Sea parte de la transformación cultural en función de la cultura del emprendimiento, creando conciencia en sus estudiantes, de la importancia del emprendimiento, con desarrollo de competencias ciudadanas, laborales, tecnológicas y para el emprendimiento, desde preescolar hasta último grado de enseñanza escolar.
+            Sea parte de la transformación cultural en función de la cultura del emprendimiento, creando conciencia en sus estudiantes de la importancia del emprendimiento, con desarrollo de competencias ciudadanas, laborales, tecnológicas y para el emprendimiento, desde preescolar hasta último grado de enseñanza escolar.
 
-            <h2 className="titulo2">Trabajando en equipo podemos lograr.</h2>
-            Trabajar en "PROYECTO DE VIDA" de sus estudiantes, para que estos a su vez se conviertan en emprendedores y generadores de riqueza, mejorando la calidad de vida de sus familias y la sociedad.
+            <h2 className="titulo2">Trabajando en equipo lo podemos lograr.</h2>
+            Trabajar en "EL PROYECTO DE VIDA" de sus estudiantes, para que estos a su vez se conviertan en emprendedores y generadores de riqueza, mejorando la calidad de vida de sus familias y la sociedad.
 
 
             </div>
--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,5 +1,7 @@
 import React from "react";
 import image from '../assets/images/valorate-transp.png';
+import logo1 from '../assets/images/LOGO_UMANIZALES_50.png';
+import logo2 from '../assets/images/logo_unidad_y_proyeccion-14.png';
 import {Link} from "react-router-dom";
 
 function TopBar(){
@@ -7,14 +9,30 @@ function TopBar(){
         <React.Fragment>
             <div className="topbar">
                 <div className="brand-icon">
-                        <Link to="/"><img src={image} alt="Valorate"/></Link>
+                        <Link to="/"><img id="valorate" src={image} alt="Valorate"/></Link>
                 </div>
                 <div className="title">
                     <div className="main">
                     Evento Cultura del Emprendimiento en Manizales
                     </div>
+                    <div className="apoya">
+                    Con el apoyo de la Universidad de Manizales y su Unidad de Emprendimiento
+
+                    </div>
                     <div className="date">
-                    Fecha: Septiembre 6 del 2022
+                    Fecha: Septiembre 6 del 2022 - Hora: 8:00 am
+                    </div>
+                    <div className="direccion">
+                    Lugar: Universidad de Manizales Auditorio Auxiliar de la Torre Emblemática 
+                    </div>
+
+                </div>
+                <div className="logo-umanizales">
+                    <div className="logo1">
+                    <img id ="logo1" src={logo1} alt="UManizales"/>
+                    </div>
+                    <div className="logo2">
+                    <img id ="logo2" src={logo2} alt="UEManizales"/>
                     </div>
 
                 </div>
